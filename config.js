8b469e74e360b4953b734d3325ff984307c60e86
Trey Felton <trey@controleverything.com>
2019-01-17 10:01:44 -0600

    Overhaul to v2

---
+++ config.js
@@ -0,0 +1,66 @@
+const sp = require('serialport');
+const inquirer = require('inquirer');
+var fs = require('fs');
+
+var prompt = inquirer.createPromptModule();
+
+module.exports = {
+	collect: selectPort,
+	opts: {
+		op: 'load',
+		port: '',
+		baudrate: '115200',
+		filename: ''
+	}
+};
+
+function selectPort(){
+	return new Promise((f, r) => {
+		sp.list().then((ports) => {
+			var names = ports.map(o => o.comName);
+			prompt([{
+				name: 'port',
+				type: 'list',
+				message: `Please select an option below, then hit enter:`,
+				choices: names
+			},{
+				name: 'baudrate',
+				type: 'list',
+				message: `Please select the baudrate for your module, then hit enter:`,
+				choices: ["115200","57600","38400","19200","9600","4800","2400","1200","600","300"],
+			},{
+				name: 'op',
+				type: 'list',
+				message: `Please select an operation to perform:`,
+				choices: [{value: "load", name: "Load Settings from File"}, {value: "save", name: "Backup Settings to File"}],
+			}]).then((r) => {
+				for(var i in r) module.exports.opts[i] = r[i];
+				if(r.op == "load"){
+					var files = fs.readdirSync('./profiles', {withFileTypes: true}).filter(v => v.split('.').pop() == 'xml');
+					if(!files){
+						//Allow them to enter full filepath to profile
+					}else{
+						prompt({
+							name: 'filename',
+							type: 'list',
+							message: 'Please select a profile from below to load, then hit enter:',
+							choices: files
+						}).then((r) => {
+							module.exports.opts.filename = __dirname + '/profiles/' + r.filename;
+							f(module.exports.opts);
+						});
+					}
+				}else{
+					prompt({
+						name: 'filename',
+						type: 'input',
+						message: 'Please enter a filename to save this profile as (without the .xml extension), and hit enter:'
+					}).then((r) => {
+						module.exports.opts.filename = __dirname + '/profiles/' + r.filename + '.xml';
+						f(module.exports.opts);
+					});
+				}
+			});
+		}).catch(console.log);
+	});
+}
--- index.js
+++ index.js
@@ -1,89 +1,130 @@
 var digi = require('./DigiParser.js');
-var comms = require('ncd-red-comm');
+const comms = require('ncd-red-comm');
 var fs = require('fs');
-var xml2js = require('xml2js');
+
 const Queue = require("promise-queue");
-var parser = new xml2js.Parser();
+const pqueue = new Queue(1);
 
-var pqueue = new Queue(1);
-// function start(usbPort, profile){
-function start(s3b, profile){
+var xml2js = require('xml2js');
 
-  //console.log("port:",usbPort);
-  console.log("profile:",profile);
-
-  // var serial = new comms.NcdSerial(usbPort, 9600);
-  // var s3b = new digi(serial);
-
-  fs.readFile(__dirname + '/profiles/'+profile+'.xml', function(err, data) {
-    parser.parseString(data, function (err, result) {
-      //console.log(result.data.profile[0].settings[0].setting);
-      var settings = result.data.profile[0].settings[0].setting;
-
-      var promises = [];
-      settings.forEach(function(element){
-        if(element._.length % 2 != 0){
-          element._ = '0'+element._;
-        }
-        var val = Buffer.from(element._, "hex");
-        //console.log(element._,[...val],val);
-        pqueue.add(() => {
-          return new Promise((fulfill, reject) => {
-            s3b.send.at_command(element.$.command, [...val]).then(()=>{
-              fulfill();
-            }).catch((err) => {
-              console.log(err);
-              reject();
-            });
-          });
-        });
-      });
-      pqueue.add(() => {
-        return new Promise((fulfill, reject) => {
-          console.log('done');
-          s3b.persist_settings().then(fulfill).catch(reject);
-        });
-      });
-      promises.forEach((p) => {
-        p.then().catch(console.log);
-      });
-    });
-  });
+const config = require('./config');
 
 
+if(process.argv[2] == 'help'){
+
+
+}else{
+	if(!process.argv[2]){
+		config.collect().then((opts) => {
+			var cmd = ["npm start", opts.op, opts.port, opts.baudrate, opts.filename].join(' ');
+			console.log('To repeat this action, simply run: ');
+			console.log("\x1b[7m"+cmd+"\x1b[0m");
+			start(opts);
+		});
+	}else{
+		start({
+			op: process.argv[2],
+			port: process.argv[3],
+			baudrate: process.argv[4],
+			filename: process.argv[5]
+		});
+	}
 }
 
-var serial = new comms.NcdSerial(process.argv[2], 115200);
-var _s3b = new digi(serial);
-
-function enableApi(s3b){
-  return new Promise((fulfill, reject) => {
-    s3b.enable_api().then(()=>{
-      s3b.persist_settings().then(fulfill).catch(reject);
-    }).catch(reject);
-  })
+function start(opts){
+	if(opts.op == 'load') load(opts.port, opts.baudrate, opts.filename);
+	else if(opts.op == 'save') save(opts.port, opts.baudrate, opts.filename);
 }
 
-serial._emitter.once('ready', () => {
-  console.log(process.argv);
-  enableApi(_s3b).then(() => {
-    start(_s3b, process.argv[3])
-  }).catch(console.log);
-});
+function save(port, br, fn){
+	var builder = new xml2js.Builder();
+	var serial = new comms.NcdSerial(port, parseInt(br));
+	var s3b = new digi(serial);
+	var backupCmds = ["CM","HP","ID","MT","PL","RR","CE","BH","NH","MR","NN","DH","DL","TO","NI","NT","NO","CI","EE","KY","BD","NB","SB","RO","FT","AP","AO","D0","D1","D2","D3","D4","D5","D6","D7","D8","D9","P0","P1","P2","P3","P4","PD","PR","M0","M1","LT","RP","AV","IC","IF","IR","SM","SO","SN","SP","ST","WH","CC","CT","GT","DD"];
+	var data = {data:{profile:[{description_file:[fn.substr(fn.lastIndexOf('/')+1)],settings:[{setting:[]}]}]}};
+	var settings = [];
+	serial._emitter.once('ready', () => {
+		s3b.enable_api().then(()=>{
+			s3b.persist_settings().then(() => {
+				backupCmds.forEach((command) => {
+					pqueue.add(() => {
+						return new Promise((fulfill, reject) => {
+							s3b.send.at_command(command).then((r)=>{
+								if(r.data){
+									var val = parseInt(r.data.map(v => ("00" + v.toString(16)).substr(-2)).join(''), 16).toString(16);
+									//console.log(command, val);
+									settings.push({'$':{command:command},'_':val});
+								}
+								fulfill();
+							}).catch((err) => {
+								console.log(err);
+								reject();
+							});
+						});
+					});
+				});
+				pqueue.add(() => {
+					return new Promise((fulfill, reject) => {
+						data.data.profile[0].settings[0].setting = settings;
+						var xml = builder.buildObject(data);
+						fs.writeFile(fn, xml, (err) => {
+							if(err) console.log(err);
+							else{
+								console.log(fn + 'successfully created.');
+							}
+							fulfill();
+							process.exit();
+						});
+					});
+				});
+			});
+		});
+	});
+}
 
-
-// function test(usbPort){
-//   var serial = new comms.NcdSerial(usbPort, 9600);
-//   serial._emitter.once('ready', () =>{
-//     setTimeout(() => {
-//       var s3b = new digi(serial);
-//       s3b.enable_api().then(()=>{
-//         s3b.persist_settings().catch(console.log);
-//       }).catch(console.log);
-//     }, 1000);
-//   });
-//
-// }
-//
-// // start(process.argv[2], process.argv[3]);
-// test(process.argv[2]);
+function load(port, br, fn){
+	var parser = new xml2js.Parser();
+	var serial = new comms.NcdSerial(port, parseInt(br));
+	var s3b = new digi(serial);
+	var cmds = [];
+	serial._emitter.once('ready', () => {
+		s3b.enable_api().then(()=>{
+			s3b.persist_settings().then(() => {
+				fs.readFile(fn, function(err, data) {
+					parser.parseString(data, function (err, result) {
+						var settings = result.data.profile[0].settings[0].setting;
+						settings.forEach(function(element){
+							if(element._.length % 2 != 0){
+								element._ = '0'+element._;
+							}
+							var val = Buffer.from(element._, "hex");
+							pqueue.add(() => {
+								return new Promise((fulfill, reject) => {
+									cmds.push(element.$.command);
+									s3b.send.at_command(element.$.command, [...val]).then(()=>{
+										fulfill();
+									}).catch((err) => {
+										console.log(err);
+										reject();
+									});
+								});
+							});
+						});
+						pqueue.add(() => {
+							return new Promise((fulfill, reject) => {
+								s3b.persist_settings().then(() => {
+									fulfill();
+									process.exit();
+								}).catch(reject);
+							});
+						});
+					});
+				});
+			}).catch((err) => {
+				console.log('Unable to persist S3B settings.');
+			});
+		}).catch((err) => {
+			console.log('Unable to enter API mode.');
+		});
+	});
+}
